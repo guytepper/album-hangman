82107c77cefd82a6d35cb15ea5ccb24affbbcc80
guytepper <guytepper@gmail.com>
2017-04-22 11:30:26 +0300

    Changed App module name to Game

---
+++ src/Game.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Game.css';
+
+class Game extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default Game;
