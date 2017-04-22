263b21888035a9774c7780f92fe823598a043c95
guytepper <guytepper@gmail.com>
2017-04-22 14:14:21 +0300

    Added logic functions

---
+++ src/Logic.js
@@ -0,0 +1,27 @@
+/**
+ * [UserGuess description]
+ */
+function UserGuess(letter) {
+
+}
+
+/**
+ * Checks if a letter exists in the word
+ */
+function isInWord(letter, word) {
+
+}
+
+/**
+ * Returns the indicies of the letter in the string
+ */
+function getIndiciesOfLetter(letter, word) {
+
+}
+
+/**
+ * Replaces underscore with the guessed letter
+ */
+function replaceUnderscore(letter) {
+
+}
