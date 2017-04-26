b10caf3e5993c7dbf23e2abff0ae99b2c4989cfe
Guy Tepper <guytepper@gmail.com>
2017-04-26 11:33:54 +0300

    Started implementing game logic inside App

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
+import {
+  createUnderscoresArr,
+  replaceUnderscores,
+  letterInWord,
+  getIndiciesOfLetter
+} from './Utils';
 import Word from './Word';
-import { Game } from './Game';
+
+const ALBUM_NAME = 'Stadium Arcadium'.toUpperCase();
+const ALBUM_NAME_ARR = [...this.ALBUM_NAME];
+const HIDDEN_LETTERS_ARRAY = createUnderscoresArr(this.ALBUM_NAME_ARR);
 
 class App extends Component {
   constructor () {
     super();
-    this.state = {};
-    this.updateHiddenWords = this.updateHiddenWords.bind(this);
+    this.keyboardPress = this.keyboardPress.bind(this);
   }
-  updateHiddenWords (newHiddenWords) {
-    this.setState({
-      HIDDEN_WORDS: newHiddenWords
-    })
+  keyboardPress (e) {
+    const word = this.ALBUM_NAME;
+    const keyCode = e.charCode || e.which;
+    if ( (keyCode >= 65 && keyCode <= 90) || (keyCode >= 97 && keyCode <= 122) ) { // TODO: Move to helper function
+      const char = String.fromCharCode(keyCode);
+      if ( letterInWord(word, char) ) {
+        const indicies = getIndiciesOfLetter(word, char);
+        this.HIDDEN_LETTERS_ARRAY = replaceUnderscores(this.HIDDEN_LETTERS_ARRAY, char, indicies);
+      }
+    }
+  }
+  componentDidMount() {
+    window.addEventListener('keydown', this.keyboardPress);
   }
   render() {
     return (
@@ -25,11 +42,6 @@ class App extends Component {
         <Word hiddenLetters={ this.state.HIDDEN_LETTERS_ARRAY } />
       </div>
     );
-  }
-  componentDidMount() {
-    // Pass
-    const gameObj = new Game(this.updateHiddenWords);
-    this.setState(gameObj);
   }
 }
 
