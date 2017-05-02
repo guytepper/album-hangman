2dc96ead5014720051cf44c61543c458812b3d36
Guy Tepper <guytepper@gmail.com>
2017-05-02 11:52:24 +0300

    Added landing component

---
+++ src/components/Landing.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+class Landing extends React.Component {
+    constructor () {
+      super();
+      this.state = {
+        username: ''        
+      };
+
+      this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    }
+
+    handleUsernameChange(event) {
+      this.setState({
+        username: event.target.value
+      })
+    }
+
+    render () {
+      return (
+        <div>
+          <h1>Album Hangman</h1>
+          <input onChange={this.handleUsernameChange} value={this.state.username} type='text' placeholder='Last.FM Username' />
+          <button>Let's play! 🤡</button>
+        </div>
+      )
+    }
+}
+
+export default Landing;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Landing from './components/Landing';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Landing />,
   document.getElementById('root')
 );
