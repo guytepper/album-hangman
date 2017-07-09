47a556bc760ccb1827fc1cdf716cffe079f3c96a
Guy Tepper <guytepper@gmail.com>
2017-07-09 19:51:12 +0300

    Fixed indention

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -4,64 +4,64 @@ import '../assets/buttons.css';
 import './Landing.css';
 
 class Landing extends React.Component {
-    constructor () {
-      super();
-      this.state = {
-        username: '',
-        period: '12month'     
-      };
+  constructor () {
+    super();
+    this.state = {
+      username: '',
+      period: '12month'     
+    };
 
-      this.handleUsernameChange = this.handleUsernameChange.bind(this);
-      this.handlePeriodChange = this.handlePeriodChange.bind(this);
-    }
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    this.handlePeriodChange = this.handlePeriodChange.bind(this);
+  }
 
-    handleUsernameChange(event) {
-      this.setState({
-        username: event.target.value
-      })
-    }
+  handleUsernameChange(event) {
+    this.setState({
+      username: event.target.value
+    })
+  }
 
-    handlePeriodChange(event) {
-      this.setState({
-        period: event.target.value
-      })
-    }
+  handlePeriodChange(event) {
+    this.setState({
+      period: event.target.value
+    })
+  }
 
-    render () {
-      return (
-        <form className='landing'>
-          <input onChange={this.handleUsernameChange} value={this.state.username} className='landing-username' type='text' placeholder='Last.FM Username' autoFocus />
+  render () {
+    return (
+      <form className='landing'>
+        <input onChange={this.handleUsernameChange} value={this.state.username} className='landing-username' type='text' placeholder='Last.FM Username' autoFocus />
 
-          <div className='landing-period'>
-            <u>Period</u>
-            <label>
-              <input type="radio" onChange={this.handlePeriodChange} name="period" className="pure-radio" value="12month" checked={this.state.period === '12month'} />
-              12 Months
-            </label>
-            <label>
-              <input type="radio" onChange={this.handlePeriodChange} name="period" className="pure-radio" value="6month" checked={this.state.period === '6month'}/>
-              6 Months
-            </label>
-            <label>
-              <input type="radio" onChange={this.handlePeriodChange} name="period" className="pure-radio" value="3month" checked={this.state.period === '3month'}/>
-              3 Months
-            </label>
-            <label>
-              <input type="radio" onChange={this.handlePeriodChange} name="period" className="pure-radio" value="1month" checked={this.state.period === '1month'}/>
-              1 Month
-            </label>
-            <label>
-              <input type="radio" onChange={this.handlePeriodChange} name="period" className="pure-radio" value="overall" checked={this.state.period === 'overall'}/>
-              All Time
-            </label>
-          </div>
+        <div className='landing-period'>
+          <u>Period</u>
+          <label>
+            <input type="radio" onChange={this.handlePeriodChange} name="period" className="pure-radio" value="12month" checked={this.state.period === '12month'} />
+            12 Months
+          </label>
+          <label>
+            <input type="radio" onChange={this.handlePeriodChange} name="period" className="pure-radio" value="6month" checked={this.state.period === '6month'}/>
+            6 Months
+          </label>
+          <label>
+            <input type="radio" onChange={this.handlePeriodChange} name="period" className="pure-radio" value="3month" checked={this.state.period === '3month'}/>
+            3 Months
+          </label>
+          <label>
+            <input type="radio" onChange={this.handlePeriodChange} name="period" className="pure-radio" value="1month" checked={this.state.period === '1month'}/>
+            1 Month
+          </label>
+          <label>
+            <input type="radio" onChange={this.handlePeriodChange} name="period" className="pure-radio" value="overall" checked={this.state.period === 'overall'}/>
+            All Time
+          </label>
+        </div>
 
-          <Link to={`/game/${this.state.username}/${this.state.period}`}>
-            <button className='landing-btn button-success pure-button' disabled={!this.state.username}>Let's play! 🤡</button>
-          </Link>
-        </form>
-      )
-    }
+        <Link to={`/game/${this.state.username}/${this.state.period}`}>
+          <button className='landing-btn button-success pure-button' disabled={!this.state.username}>Let's play! 🤡</button>
+        </Link>
+      </form>
+    )
+  }
 }
 
 export default Landing;
