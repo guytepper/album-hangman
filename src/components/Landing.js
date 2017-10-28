98778ec2801db2a1911233b7dd24f0644b2531dc
guytepper <guytepper@gmail.com>
2017-10-28 18:07:56 +0300

    Lifted state from Landing to App.js

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,128 +3,100 @@ import { Link } from 'react-router-dom';
 import '../assets/buttons.css';
 import './Landing.css';
 
-class Landing extends React.Component {
-  state = {
-    username: '',
-    period: '12month',
-    hideArtwork: false
-  };
+function Landing(props) {
+  console.log(props);
 
-  handleUsernameChange = event => {
-    this.setState({
-      username: event.target.value
-    });
-  };
-
-  handlePeriodChange = event => {
-    this.setState({
-      period: event.target.value
-    });
-  };
-
-  handleHideArtworkChange = event => {
-    this.setState({
-      hideArtwork: event.target.checked
-    });
-  };
-
-  render() {
-    let dest = `/game/${this.state.username}/${this.state.period}`;
-    if (this.state.hideArtwork) dest += '/hard';
-
-    return (
-      <form className="landing">
-        <input
-          onChange={this.handleUsernameChange}
-          value={this.state.username}
-          className="landing-username"
-          type="text"
-          placeholder="Last.FM Username"
-          autoFocus
-        />
-
-        <div className="game-option">
-          <u>Period</u>
-          <label>
-            <input
-              type="radio"
-              onChange={this.handlePeriodChange}
-              name="period"
-              className="pure-radio"
-              value="12month"
-              checked={this.state.period === '12month'}
-            />
-            12 Months
-          </label>
-          <label>
-            <input
-              type="radio"
-              onChange={this.handlePeriodChange}
-              name="period"
-              className="pure-radio"
-              value="6month"
-              checked={this.state.period === '6month'}
-            />
-            6 Months
-          </label>
-          <label>
-            <input
-              type="radio"
-              onChange={this.handlePeriodChange}
-              name="period"
-              className="pure-radio"
-              value="3month"
-              checked={this.state.period === '3month'}
-            />
-            3 Months
-          </label>
-          <label>
-            <input
-              type="radio"
-              onChange={this.handlePeriodChange}
-              name="period"
-              className="pure-radio"
-              value="1month"
-              checked={this.state.period === '1month'}
-            />
-            1 Month
-          </label>
-          <label>
-            <input
-              type="radio"
-              onChange={this.handlePeriodChange}
-              name="period"
-              className="pure-radio"
-              value="overall"
-              checked={this.state.period === 'overall'}
-            />
-            All Time
-          </label>
-        </div>
-
-        <div className="game-option">
-          <u>Advanced</u>
-          <label>
-            <input
-              type="checkbox"
-              onChange={this.handleHideArtworkChange}
-              name="hideArtwork"
-              checked={this.state.hideArtwork}
-            />
-            Hide Artwork
-          </label>
-        </div>
-        <Link to={dest} style={!this.state.username ? { pointerEvents: 'none' } : null}>
-          <button className="button-success pure-button" disabled={!this.state.username}>
-            Let's play!{' '}
-            <span role="img" aria-label="Clown">
-              🤡
-            </span>
-          </button>
-        </Link>
-      </form>
-    );
-  }
+  return (
+    <form className="landing">
+      <input
+        onChange={e => props.updateSetting('username', e.target.value)}
+        value={props.username}
+        className="landing-username"
+        type="text"
+        placeholder="Last.FM Username"
+        autoFocus
+      />
+      <div className="game-option">
+        <u>Period</u>
+        <label>
+          <input
+            type="radio"
+            onChange={e => props.updateSetting('period', e.target.value)}
+            name="period"
+            className="game-option-radio"
+            value="12month"
+            checked={props.period === '12month'}
+          />
+          12 Months
+        </label>
+        <label>
+          <input
+            type="radio"
+            onChange={e => props.updateSetting('period', e.target.value)}
+            name="period"
+            className="game-option-radio"
+            value="6month"
+            checked={props.period === '6month'}
+          />
+          6 Months
+        </label>
+        <label>
+          <input
+            type="radio"
+            onChange={e => props.updateSetting('period', e.target.value)}
+            name="period"
+            className="game-option-radio"
+            value="3month"
+            checked={props.period === '3month'}
+          />
+          3 Months
+        </label>
+        <label>
+          <input
+            type="radio"
+            onChange={e => props.updateSetting('period', e.target.value)}
+            name="period"
+            className="game-option-radio"
+            value="1month"
+            checked={props.period === '1month'}
+          />
+          1 Month
+        </label>
+        <label>
+          <input
+            type="radio"
+            onChange={e => props.updateSetting('period', e.target.value)}
+            name="period"
+            className="game-option-radio"
+            value="overall"
+            checked={props.period === 'overall'}
+          />
+          All Time
+        </label>
+      </div>
+      <div className="game-option">
+        <u>Advanced</u>
+        <label className>
+          <input
+            type="checkbox"
+            className="game-option-checkbox"
+            onChange={e => props.updateSetting('hideArtwork', e.target.checked)}
+            name="hideArtwork"
+            checked={props.hideArtwork}
+          />
+          Hide Artwork
+        </label>
+      </div>
+      <Link to={`/game/${props.username}/${props.period}`}>
+        <button className="button-success pure-button" disabled={!props.username}>
+          Let's play!&nbsp;
+          <span role="img" aria-label="Clown">
+            🤡
+          </span>
+        </button>
+      </Link>
+    </form>
+  );
 }
 
 export default Landing;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-
-import Game from './Game';
-import Landing from './components/Landing';
 import './index.css';
+import App from './App';
 
-ReactDOM.render(
-  <Router>
-    <div className="app">
-      <h1>Album Hangman</h1>
-      <div className="container">
-        <Route exact path="/" component={Landing} />
-        <Route path="/game/:username/:period?/:hideArtwork?" component={Game} />
-      </div>
-      <footer>
-        <a href="https://github.com/guytepper/album-hangman">
-          <img src="/github.svg" alt="View on GitHub" className="github-icon" />
-        </a>
-      </footer>
-    </div>
-  </Router>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
