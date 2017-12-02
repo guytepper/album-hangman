63395ef3bcc29284707f242f55af39f068eb71a0
guytepper <guytepper@gmail.com>
2017-12-02 18:16:49 +0200

    Improved test

--- src/App.test.js
+++ src/App.test.js
@@ -9,6 +9,14 @@ it('renders without crashing', () => {
 it('updates setting value', () => {
   const wrapper = shallow(<App />);
   expect(wrapper.state().username).toEqual('');
+  expect(wrapper.state().period).toEqual('12month');
+  expect(wrapper.state().hideArtwork).toEqual(false);
+
   wrapper.instance().updateSetting('username', 'Dobida');
+  wrapper.instance().updateSetting('period', 'overall');
+  wrapper.instance().updateSetting('hideArtwork', true);
+
   expect(wrapper.state().username).toEqual('Dobida');
+  expect(wrapper.state().period).toEqual('overall');
+  expect(wrapper.state().hideArtwork).toEqual(true);
 });
