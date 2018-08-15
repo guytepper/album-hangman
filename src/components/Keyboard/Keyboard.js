27085d370224b6f45c336039166760f28f8bbb38
Guy Tepper <guytepper@gmail.com>
2018-08-15 19:13:45 +0300

    Removed constructor from Keyboard component

--- src/components/Keyboard/Keyboard.js
+++ src/components/Keyboard/Keyboard.js
@@ -4,23 +4,17 @@ import './Keyboard.css';
 const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 
 class Keyboard extends React.Component {
-  constructor() {
-    super();
-    this.getButton = this.getButton.bind(this);
-    this.handlePress = this.handlePress.bind(this);
-  }
-
-  getButton(letter) {
+  getButton = letter => {
     return (
       <button key={letter} onClick={() => this.handlePress(letter)} className="keyboard-btn">
         {letter}
       </button>
     );
-  }
+  };
 
-  handlePress(letter) {
+  handlePress = letter => {
     this.props.onPress(letter);
-  }
+  };
 
   render() {
     return <div className="keyboard">{letters.map(letter => this.getButton(letter))}</div>;
