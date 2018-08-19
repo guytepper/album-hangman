2022b42938015d7d0421e68a03e84109c8eb5b4a
guytepper <guytepper@gmail.com>
2018-08-19 12:30:16 +0300

    Improved responsiveness for larger screens

--- src/views/Game/Game.js
+++ src/views/Game/Game.js
@@ -175,13 +175,15 @@ class Game extends Component {
         <Link to="/" className="back-button-link">
           <img src="/back.svg" alt="" className="back-button" />
         </Link>
-        <Artwork
-          img={this.state.currentAlbum.image}
-          blurLevel={(4 - this.state.currentGame.failedGuesses) * 10}
-          gameEnd={this.gameEnd()}
-          hidden={this.hideArtwork}
-        />
-        <Word hiddenLetters={this.state.currentGame.hiddenWord} />
+        <div className="game-stage">
+          <Artwork
+            img={this.state.currentAlbum.image}
+            blurLevel={(4 - this.state.currentGame.failedGuesses) * 10}
+            gameEnd={this.gameEnd()}
+            hidden={this.hideArtwork}
+          />
+          <Word hiddenLetters={this.state.currentGame.hiddenWord} />
+        </div>
         <div className="game-stats">
           <Hearts lives={4 - this.state.currentGame.failedGuesses} />
           <GuessedLetters letters={this.state.currentGame.failedLetters} />
