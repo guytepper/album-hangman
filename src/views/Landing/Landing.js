82c488a1e3f62222c8b3d98c2e3fc9c731a912d1
guytepper <guytepper@gmail.com>
2018-09-10 14:29:50 +0300

    Added login button

--- src/views/Landing/Landing.js
+++ src/views/Landing/Landing.js
@@ -5,6 +5,11 @@ function Landing(props) {
   return (
     <div className="landing">
       <h1 className="landing-header">Album Hangman</h1>
+      <p>Can you guess your favorite Spotify albums?</p>
+      <a className="spotify-button">
+        <img alt="Spotify" src="/spotify.svg" className="spotify-button-logo" />
+        <span>Connect with Spotify</span>
+      </a>
     </div>
   );
 }
