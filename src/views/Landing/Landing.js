ca7f36c14387cee4b670d55a540ed9b154e5f932
guytepper <guytepper@gmail.com>
2018-09-10 14:29:43 +0300

    Removed landing form

--- src/views/Landing/Landing.js
+++ src/views/Landing/Landing.js
@@ -1,104 +1,10 @@
 import React from 'react';
-import Button from '../../components/Button';
 import './Landing.css';
 
 function Landing(props) {
   return (
     <div className="landing">
       <h1 className="landing-header">Album Hangman</h1>
-      <p>Can you guess your top Last.FM albums?</p>
-      <form className="landing-form">
-        <input
-          onChange={e => props.updateSetting('username', e.target.value)}
-          value={props.username}
-          className="landing-username"
-          type="text"
-          placeholder="Last.FM Username"
-          autoFocus
-        />
-        <div className="game-options">
-          <div className="game-option">
-            <u>Period</u>
-            <label>
-              <input
-                type="radio"
-                onChange={e => props.updateSetting('period', e.target.value)}
-                name="period"
-                className="game-option-radio"
-                value="12month"
-                checked={props.period === '12month'}
-              />
-              12 Months
-            </label>
-            <label>
-              <input
-                type="radio"
-                onChange={e => props.updateSetting('period', e.target.value)}
-                name="period"
-                className="game-option-radio"
-                value="6month"
-                checked={props.period === '6month'}
-              />
-              6 Months
-            </label>
-            <label>
-              <input
-                type="radio"
-                onChange={e => props.updateSetting('period', e.target.value)}
-                name="period"
-                className="game-option-radio"
-                value="3month"
-                checked={props.period === '3month'}
-              />
-              3 Months
-            </label>
-            <label>
-              <input
-                type="radio"
-                onChange={e => props.updateSetting('period', e.target.value)}
-                name="period"
-                className="game-option-radio"
-                value="1month"
-                checked={props.period === '1month'}
-              />
-              1 Month
-            </label>
-            <label>
-              <input
-                type="radio"
-                onChange={e => props.updateSetting('period', e.target.value)}
-                name="period"
-                className="game-option-radio"
-                value="overall"
-                checked={props.period === 'overall'}
-              />
-              All Time
-            </label>
-          </div>
-          <div className="game-option">
-            <u>Advanced</u>
-            <label>
-              <input
-                type="checkbox"
-                className="game-option-checkbox"
-                onChange={e => props.updateSetting('hideArtwork', e.target.checked)}
-                name="hideArtwork"
-                checked={props.hideArtwork}
-              />
-              Hide Artwork
-            </label>
-          </div>
-        </div>
-        <Button
-          onClick={() => props.history.push(`/game/${props.username}/${props.period}`)}
-          disabled={!props.username}
-        >
-          Let's play!{' '}
-          <span role="img" aria-label="Clown">
-            🤡
-          </span>
-        </Button>
-      </form>
     </div>
   );
 }
