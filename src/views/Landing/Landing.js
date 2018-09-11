0882f98dda4d7581d12f9b0544ae2df4be9586d9
guytepper <guytepper@gmail.com>
2018-09-11 17:22:06 +0300

    Add link to authorization request

--- src/views/Landing/Landing.js
+++ src/views/Landing/Landing.js
@@ -6,7 +6,10 @@ function Landing(props) {
     <div className="landing">
       <h1 className="landing-header">Album Hangman</h1>
       <p>Can you guess your favorite Spotify albums?</p>
-      <a className="spotify-button">
+      <a
+        className="spotify-button"
+        href="https://accounts.spotify.com/authorize?client_id=b50c5fa3329f4d1cb16d82142c82654c&response_type=token&scope=user-library-read&redirect_uri=http://localhost:3000/game/"
+      >
         <img alt="Spotify" src="/spotify.svg" className="spotify-button-logo" />
         <span>Connect with Spotify</span>
       </a>
