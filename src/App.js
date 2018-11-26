9dd8a14f566a3b9b13cbc6bafd600be4102dfe07
Guy Tepper <guytepper@gmail.com>
2018-11-26 14:53:18 +0200

    Moved service authorization from App to Landing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-
 import Game from './views/Game';
 import Landing from './views/Landing';
 
@@ -10,13 +9,6 @@ class App extends Component {
   };
 
   selectService = service => {
-    if (service === 'spotify') {
-      window.open(
-        'https://accounts.spotify.com/authorize?client_id=b50c5fa3329f4d1cb16d82142c82654c&response_type=token&scope=user-library-read&redirect_uri=http://localhost:3000/game/',
-        '_self'
-      );
-    }
-
     this.setState({ service });
   };
 
--- src/views/Landing/Landing.js
+++ src/views/Landing/Landing.js
@@ -1,21 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Landing.css';
 
-function Landing(props) {
-  return (
-    <div className="landing">
-      <h1 className="landing-header">Album Hangman</h1>
-      <p>Can you guess your favorite Spotify albums?</p>
-      <a onClick={() => props.selectService('spotify')} className="spotify-button">
-        <img alt="Spotify" src="/spotify.svg" className="spotify-button-logo" />
-        <span>Connect with Spotify</span>
-      </a>
-      <a className="spotify-button" onClick={() => props.selectService('appleMusic')}>
-        <img alt="Apple Music" src="/apple_music.png" className="spotify-button-logo" />
-        <span>Connect with Apple Music</span>
-      </a>
-    </div>
-  );
+class Landing extends Component {
+  selectSpotify = () => {
+    this.props.selectService('spotify');
+    window.open(
+      'https://accounts.spotify.com/authorize?client_id=b50c5fa3329f4d1cb16d82142c82654c&response_type=token&scope=user-library-read&redirect_uri=http://localhost:3000/game/',
+      '_self'
+    );
+  };
+
+  selectAppleMusic = () => {
+    this.props.selectService('appleMusic');
+    window.MusicKit.configure({
+    });
+    const musicKit = window.MusicKit.getInstance();
+    musicKit.authorize().then(() => {
+      this.props.history.push('/game/');
+    });
+  };
+
+  render() {
+    return (
+      <div className="landing">
+        <h1 className="landing-header">Album Hangman</h1>
+        <p>Can you guess your favorite Spotify albums?</p>
+        <a onClick={this.selectSpotify} className="spotify-button">
+          <img alt="Spotify" src="/spotify.svg" className="spotify-button-logo" />
+          <span>Connect with Spotify</span>
+        </a>
+        <a className="spotify-button" onClick={this.selectAppleMusic}>
+          <img alt="Apple Music" src="/apple_music.png" className="spotify-button-logo" />
+          <span>Connect with Apple Music</span>
+        </a>
+      </div>
+    );
+  }
 }
 
 export default Landing;
