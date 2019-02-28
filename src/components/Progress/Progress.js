3ff326541949c6b9f8f094af00f46f6bb161e5b7
Guy Tepper <guytepper@gmail.com>
2019-02-28 16:03:13 +0200

    Added thin progress design

--- src/components/Progress/Progress.js
+++ src/components/Progress/Progress.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Progress.css';
 
-function Progress() {
+function Progress({}) {
   return (
-    <div className="progress-wrap progress">
-      <div className="progress-bar progress" />
-      <span className="progress-label">PROGRESS: 52 / 120</span>
+    <div className="progress-container">
+      <span className="progress-thin-label">PROGRESS: 52 / 120</span>
+      <div className="progress-wrap progress">
+        <span className="progress-label">PROGRESS: 52 / 120</span>
+        <div className="progress-bar progress" />
+      </div>
     </div>
   );
 }
