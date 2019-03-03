5e6ae1bd5ccde29f9a4e3b53f975222fc52ce791
Guy Tepper <guytepper@gmail.com>
2019-03-03 12:48:43 +0200

    Display progress and total numbers

--- src/components/Progress/Progress.js
+++ src/components/Progress/Progress.js
@@ -7,7 +7,9 @@ function Progress({ type, progress, total }) {
   if (type === 'thin') {
     return (
       <div className="progress-container">
-        <span className="progress-thin-label">PROGRESS: 52 / 120</span>
+        <span className="progress-thin-label">
+          PROGRESS: {progress} / {total}
+        </span>
         <div className="progress-wrap progress-thin">
           <div className="progress-bar progress-thin" style={{ width: PERCENT + '%' }} />
         </div>
@@ -18,7 +20,9 @@ function Progress({ type, progress, total }) {
   return (
     <div className="progress-container">
       <div className="progress-wrap progress">
-        <span className="progress-label">PROGRESS: 52 / 120</span>
+        <span className="progress-label">
+          PROGRESS: {progress} / {total}
+        </span>
         <div className="progress-bar progress" style={{ width: PERCENT + '%' }} />
       </div>
     </div>
