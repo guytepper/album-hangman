0ea9c2e17ef0aea2a8867baf4f74fa16c26c7b73
Guy Tepper <guytepper@gmail.com>
2019-03-03 12:37:31 +0200

    Added total and progress props

--- src/components/Progress/Progress.js
+++ src/components/Progress/Progress.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './Progress.css';
 
-function Progress({ type }) {
+function Progress({ type, progress, total }) {
+  const PERCENT = (progress / total) * 100;
+
   if (type === 'thin') {
     return (
       <div className="progress-container">
         <span className="progress-thin-label">PROGRESS: 52 / 120</span>
         <div className="progress-wrap progress-thin">
-          <div className="progress-bar progress-thin" />
+          <div className="progress-bar progress-thin" style={{ width: PERCENT + '%' }} />
         </div>
       </div>
     );
@@ -17,7 +19,7 @@ function Progress({ type }) {
     <div className="progress-container">
       <div className="progress-wrap progress">
         <span className="progress-label">PROGRESS: 52 / 120</span>
-        <div className="progress-bar progress" />
+        <div className="progress-bar progress" style={{ width: PERCENT + '%' }} />
       </div>
     </div>
   );
--- src/components/Progress/Progress.stories.js
+++ src/components/Progress/Progress.stories.js
@@ -3,5 +3,5 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import Progress from './Progress';
 
-storiesOf('Progress', module).add('standard', () => <Progress />);
-storiesOf('Progress', module).add('thin', () => <Progress type="thin" />);
+storiesOf('Progress', module).add('standard', () => <Progress total={152} progress={33} />);
+storiesOf('Progress', module).add('thin', () => <Progress total={152} progress={33} type="thin" />);
