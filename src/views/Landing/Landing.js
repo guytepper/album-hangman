13ddac03096e30ebcd55f1fe436d44a031ccca4c
Guy Tepper <guytepper@gmail.com>
2019-03-05 21:04:52 +0200

    Improvements 😅

--- src/views/Landing/Landing.js
+++ src/views/Landing/Landing.js
@@ -38,8 +38,7 @@ class Landing extends Component {
       <div className="landing">
         <h1 className="landing-header">Do you really know your music?</h1>
         <div className="landing-moving-albums">
-          <img className="landing-moving-albums-image" src="./albums-carousel1.png" />
-          <img className="landing-moving-albums-image" src="./albums-carousel1.png" />
+          <img className="landing-moving-albums-image" src="./albums-carousel1.jpg" />
         </div>
         <div className="landing-game-info">
           <div className="landing-description">
@@ -47,15 +46,26 @@ class Landing extends Component {
               We all know Hangman. It’s a classic game. But let’s see if you can beat it with your own music taste.
             </p>
             <p>
-              Each round a blurred album artwork from your music library would be displayed: will you be able to guess
-              the name of the album?
+              Each round a blurred album artwork from your music library would be displayed: <br />
+              will you be able to guess the name of the album?
             </p>
           </div>
-          <div>
+          <div className="login-buttons">
             <LoginButton type="Spotify" icon="/spotify.svg" onClick={this.selectSpotify} />
             <LoginButton type="Apple Music" icon="/apple_music.png" onClick={this.selectAppleMusic} />
           </div>
         </div>
+        <footer className="landing-footer">
+          <span className="landing-footer-created">
+            Created by <span className="landing-footer-created-name">Guy Tepper</span>
+          </span>
+          <span>
+            Contribute on{' '}
+            <a className="landing-footer-github-link" href="https://github.com/guytepper/album-hangman">
+              Github
+            </a>
+          </span>
+        </footer>
       </div>
     );
   }
