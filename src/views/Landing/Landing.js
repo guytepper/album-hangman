8f83895f8c29c0218cef7f851a4a6bbd55a7a461
Guy Tepper <guytepper@gmail.com>
2019-03-06 14:56:04 +0200

    Removed class from landing

--- src/views/Landing/Landing.js
+++ src/views/Landing/Landing.js
@@ -7,68 +7,70 @@ if (process.env.NODE_ENV === 'production') {
   spotifyRedirectURL = 'https://album-hangman.com/game/';
 }
 
-class Landing extends Component {
-  selectSpotify = () => {
-    this.props.selectService('spotify');
-    window.open(
-      `https://accounts.spotify.com/authorize?client_id=${
-        process.env.REACT_APP_SPOTIFY_ID
-      }&response_type=token&scope=user-library-read&redirect_uri=${spotifyRedirectURL}`,
-      '_self'
-    );
-  };
+function selectSpotify(selectService) {
+  selectService('spotify');
+  window.open(
+    `https://accounts.spotify.com/authorize?client_id=${
+      process.env.REACT_APP_SPOTIFY_ID
+    }&response_type=token&scope=user-library-read&redirect_uri=${spotifyRedirectURL}`,
+    '_self'
+  );
+}
 
-  selectAppleMusic = () => {
-    this.props.selectService('appleMusic');
-    window.MusicKit.configure({
-      developerToken: process.env.REACT_APP_MUSICKIT_TOKEN,
-      app: {
-        name: 'Album Hangman',
-        build: '2018.29.11'
-      }
-    });
-    const musicKit = window.MusicKit.getInstance();
-    musicKit.authorize().then(() => {
-      this.props.history.push('/game/');
-    });
-  };
+function selectAppleMusic(selectService) {
+  selectService('appleMusic');
+  window.MusicKit.configure({
+    developerToken: process.env.REACT_APP_MUSICKIT_TOKEN,
+    app: {
+      name: 'Album Hangman',
+      build: '2018.29.11'
+    }
+  });
+  const musicKit = window.MusicKit.getInstance();
+  musicKit.authorize().then(() => {
+    this.props.history.push('/game/');
+  });
+}
 
-  render() {
-    return (
-      <div className="landing">
-        <h1 className="landing-header">Do you really know your music?</h1>
-        <div className="landing-moving-albums">
-          <img className="landing-moving-albums-image" src="./albums-carousel2.jpg" />
-        </div>
-        <div className="landing-game-info">
-          <div className="landing-description">
-            <p className="bold-text">
-              We all know Hangman. It’s a classic game. But let’s see if you can beat it with your own music taste.
-            </p>
-            <p>
-              Each round a blurred album artwork from your music library will be displayed: would you be able to guess
-              the name of the album?
-            </p>
-          </div>
-          <div className="login-buttons">
-            <LoginButton type="Spotify" icon="/spotify.svg" onClick={this.selectSpotify} />
-            <LoginButton type="Apple Music" icon="/apple_music.png" onClick={this.selectAppleMusic} />
-          </div>
-        </div>
-        <footer className="landing-footer">
-          <span className="landing-footer-created">
-            Created by <span className="landing-footer-created-name">Guy Tepper</span>
-          </span>
-          <span>
-            Fork me on{' '}
-            <a className="landing-footer-github-link" href="https://github.com/guytepper/album-hangman">
-              Github
-            </a>
-          </span>
-        </footer>
+function Landing(props) {
+  return (
+    <div className="landing">
+      <h1 className="landing-header">Do you really know your music?</h1>
+      <div className="landing-moving-albums">
+        <img className="landing-moving-albums-image" src="./albums-carousel2.jpg" />
       </div>
-    );
-  }
+      <div className="landing-game-info">
+        <div className="landing-description">
+          <p className="bold-text">
+            We all know Hangman. It’s a classic game. But let’s see if you can beat it with your own music taste.
+          </p>
+          <p>
+            Each round a blurred album artwork from your music library will be displayed: would you be able to guess the
+            name of the album?
+          </p>
+        </div>
+        <div className="login-buttons">
+          <LoginButton type="Spotify" icon="/spotify.svg" onClick={() => selectSpotify(props.selectService)} />
+          <LoginButton
+            type="Apple Music"
+            icon="/apple_music.png"
+            onClick={() => selectAppleMusic(props.selectService)}
+          />
+        </div>
+      </div>
+      <footer className="landing-footer">
+        <span className="landing-footer-created">
+          Created by <span className="landing-footer-created-name">Guy Tepper</span>
+        </span>
+        <span>
+          Fork me on{' '}
+          <a className="landing-footer-github-link" href="https://github.com/guytepper/album-hangman">
+            Github
+          </a>
+        </span>
+      </footer>
+    </div>
+  );
 }
 
 export default Landing;
