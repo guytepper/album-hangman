fcc8af5a0c3b4b2987afcd25be4a027c358b2575
Guy Tepper <guytepper@gmail.com>
2019-03-07 13:52:18 +0200

    Changed button design

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactLoading from 'react-loading';
 import './Button.css';
 
+let btnTypeClassName = '';
 export default function Button(props) {
+  if (props.type) btnTypeClassName = `hangman-btn-${props.type}`;
+
   return (
-    <button onClick={props.onClick} disabled={props.disabled} className="hangman-btn primary-btn">
+    <button onClick={props.onClick} disabled={props.disabled} className={`hangman-btn ${btnTypeClassName}`}>
       <div className="button-wrapper">
         <span className="button-loading-wrapper">
           {props.loading && <ReactLoading type="spin" width={20} height={20} />}
