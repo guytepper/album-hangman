57f680871a4a96a24b7bf35a7f5eb4a1dc16b320
guytepper <guytepper@gmail.com>
2019-03-19 11:49:48 +0200

    Updated prop

--- src/components/LoginButton/LoginButton.stories.js
+++ src/components/LoginButton/LoginButton.stories.js
@@ -5,5 +5,5 @@ import LoginButton from './LoginButton';
 
 storiesOf('Login Button', module).add('spotify', () => <LoginButton text="Connect with Spotify" icon="/spotify.svg" />);
 storiesOf('Login Button', module).add('apple music', () => (
-  <LoginButton type="Connect Apple Music" icon="/apple_music.png" />
+  <LoginButton text="Connect with Apple Music" icon="/apple_music.png" />
 ));
