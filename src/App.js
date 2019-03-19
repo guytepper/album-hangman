6b4c3707baca6cbfc477fd6df665d8368e37d107
guytepper <guytepper@gmail.com>
2019-03-19 20:19:21 +0200

    Added route transitions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import Game from './views/Game';
 import Landing from './views/Landing';
 
@@ -12,10 +13,22 @@ function App() {
 
   return (
     <Router>
-      <React.Fragment>
-        <Route exact path="/" render={props => <Landing {...props} selectService={service => setService(service)} />} />
-        <Route path="/game/" render={props => <Game {...props} service={service} />} />
-      </React.Fragment>
+      <Route
+        render={({ location }) => (
+          <TransitionGroup>
+            <CSSTransition classNames="fade" timeout={1000} key={location.key}>
+              <Switch location={location}>
+                <Route
+                  exact
+                  path="/"
+                  render={props => <Landing {...props} selectService={service => setService(service)} />}
+                />
+                <Route path="/game/" render={props => <Game {...props} service={service} />} />
+              </Switch>
+            </CSSTransition>
+          </TransitionGroup>
+        )}
+      />
     </Router>
   );
 }
