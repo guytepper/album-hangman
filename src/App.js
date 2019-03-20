116b17e9018dbaad85ea642454e07236313ae9e3
guytepper <guytepper@gmail.com>
2019-03-20 10:08:23 +0200

    Removed router animation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Game from './views/Game';
 import Landing from './views/Landing';
 
 function App() {
   return (
     <Router>
-      <Route
-        render={({ location }) => (
-          <TransitionGroup>
-            <CSSTransition classNames="fade" timeout={1000} key={location.key}>
-              <Switch location={location}>
-                <Route exact path="/" render={props => <Landing {...props} />} />
-                <Route path="/game/" render={props => <Game {...props} />} />
-              </Switch>
-            </CSSTransition>
-          </TransitionGroup>
-        )}
-      />
+      <React.Fragment>
+        <Route exact path="/" render={props => <Landing {...props} />} />
+        <Route path="/game/" render={props => <Game {...props} />} />
+      </React.Fragment>
     </Router>
   );
 }
