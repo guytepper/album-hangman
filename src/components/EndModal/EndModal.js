034f43e60a2e96d800f8f9737262ea3168f948fa
Guy Tepper <guytepper@gmail.com>
2019-03-26 22:02:46 +0200

    Added facebook link

--- src/components/EndModal/EndModal.js
+++ src/components/EndModal/EndModal.js
@@ -4,7 +4,9 @@ import Button from '../Button';
 import './EndModal.css';
 
 const twitterLink = `https://twitter.com/intent/tweet?text=Apparently%20I%20do%20know%20my%20music.%20What%20about%20you?&url=https://album-hangman.com`;
-const facebookLink = `https://www.facebook.com/sharer/sharer.php?u=https%3A//me.com`;
+const facebookLink = `https://www.facebook.com/sharer/sharer.php?u=https://album-hangman.com`;
+const popupSettings = `menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=555`;
+
 function EndModal() {
   return (
     <Modal className="end-modal">
@@ -14,15 +16,17 @@ function EndModal() {
         <p>Now go on and brag to your friends!</p>
       </div>
       <div className="end-modal-buttons">
-        <Button type="primary" className="end-modal-facebook-btn">
+        <Button
+          type="primary"
+          className="end-modal-facebook-btn"
+          onClick={() => window.open(facebookLink, '', popupSettings)}
+        >
           Share on Facebook
         </Button>
         <Button
           type="info"
           className="end-modal-twitter-btn"
-          onClick={() =>
-            window.open(twitterLink, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600')
-          }
+          onClick={() => window.open(twitterLink, '', popupSettings)}
         >
           Share on Twitter
         </Button>
