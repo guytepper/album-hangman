d41d4c813d020f32c3eb4ec68d916211196582ca
guytepper <guytepper@gmail.com>
2019-03-26 20:54:33 +0200

    Added tweet link

--- src/components/EndModal/EndModal.js
+++ src/components/EndModal/EndModal.js
@@ -3,6 +3,8 @@ import Modal from '../Modal';
 import Button from '../Button';
 import './EndModal.css';
 
+const tweetLink = `https://twitter.com/intent/tweet?text=Apparently%20I%20do%20know%20my%20music.%20What%20about%20you?&url=https://album-hangman.com`;
+
 const facebookLink = `https://www.facebook.com/dialog/share?
 app_id=265726350983437&
 display=popup&
@@ -26,7 +28,13 @@ function EndModal() {
         <Button type="primary" className="end-modal-facebook-btn">
           Share on Facebook
         </Button>
-        <Button type="info" className="end-modal-twitter-btn">
+        <Button
+          type="info"
+          className="end-modal-twitter-btn"
+          onClick={() =>
+            window.open(tweetLink, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600')
+          }
+        >
           Share on Twitter
         </Button>
         <Button type="success">Play Again</Button>
