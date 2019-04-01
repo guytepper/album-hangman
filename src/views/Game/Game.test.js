41aaf980c20025030ca1fa9406810c4230999dc2
Guy Tepper <guytepper@gmail.com>
2019-04-01 14:14:15 +0300

    New game method test (wip)

--- src/views/Game/Game.test.js
+++ src/views/Game/Game.test.js
@@ -89,6 +89,22 @@ it('displays settings modal on gear icon click', () => {
   expect(mainWrapper.state().displaySettings).toBeTruthy();
 });
 
+it('starts new game method calls correct methods', () => {
+  const setNewAlbumMock = jest.fn();
+  wrapper = mount(<Game loading={true} totalAlbums={100} progress={0} nextAlbum={null} error={null} />);
+
+  wrapper.instance().setNewAlbum = setNewAlbumMock;
+  wrapper.update();
+  wrapper.instance().handleLetterPress('T');
+  wrapper.instance().handleLetterPress('i');
+  wrapper.instance().handleLetterPress('d');
+  wrapper.instance().handleLetterPress('a');
+  wrapper.instance().handleLetterPress('l');
+
+  expect(setNewAlbumMock).toBeCalled();
+  console.log(wrapper.state());
+});
+
 it('displays end modal on game end', () => {
   const wrapper = mount(<Game totalAlbums={100} progress={100} loading={true} />);
   jest.useFakeTimers();
