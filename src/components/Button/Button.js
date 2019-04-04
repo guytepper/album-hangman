ec55794f5f364b4eeaedacfdd5734708d2ae780d
guytepper <guytepper@gmail.com>
2019-04-04 14:58:22 +0300

    Removed button loading option
    Since it’s not being used

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactLoading from 'react-loading';
 import './Button.css';
 
 let btnTypeClassName = '';
@@ -10,9 +9,6 @@ export default function Button(props) {
   return (
     <button onClick={props.onClick} disabled={props.disabled} className={`hangman-btn ${btnTypeClassName}`}>
       <div className="button-wrapper">
-        <span className="button-loading-wrapper">
-          {props.loading && <ReactLoading type="spin" width={20} height={20} />}
-        </span>
         <span className="button-text-wrapper">{props.children}</span>
       </div>
     </button>
