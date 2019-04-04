90438b9e1dd0fc7aede1882c41ff431daf0de1e4
guytepper <guytepper@gmail.com>
2019-04-04 23:00:30 +0300

    Added Share button
    using web share api

--- src/components/SettingsModal/SettingsModal.js
+++ src/components/SettingsModal/SettingsModal.js
@@ -3,9 +3,19 @@ import Modal from '../Modal';
 import Button from '../Button';
 import './SettingsModal.css';
 
+const shareInfo = {
+  title: 'Album Hangman',
+  text: 'Check out this game!',
+  url: 'https://album-hangman.com'
+};
+
+let displayShareBtn = false;
+if (window.navigator.share) {
+  displayShareBtn = true;
+}
+
 function SettingsModal({ resetGameProgress, setSettingsDisplay }) {
   return (
-    <Modal>
     <Modal className="settings-modal">
       <span className="settings-modal-text">
         Your progress is being saved - you'll be able to continue playing later if you'll close the window.
@@ -14,6 +24,11 @@ function SettingsModal({ resetGameProgress, setSettingsDisplay }) {
         <Button type="warning" onClick={resetGameProgress}>
           Reset Progress
         </Button>
+        {displayShareBtn && (
+          <Button type="info" onClick={() => navigator.share(shareInfo)}>
+            Share
+          </Button>
+        )}
         <Button type="success" onClick={() => setSettingsDisplay(false)}>
           Close
         </Button>
