e713d9610642bf3b7ee72b950d4d7650d3a81a1b
guytepper <guytepper@gmail.com>
2019-04-05 18:06:27 +0300

    Added error detail

--- src/views/Error/Error.js
+++ src/views/Error/Error.js
@@ -3,10 +3,21 @@ import { Link } from 'react-router-dom';
 import Button from '../../components/Button';
 import './Error.css';
 
+const errorDetail = 'Spotify is providing only albums you saved in full - not invidual songs.';
+
 function Error({ message }) {
+  let showDetail = false;
+  if (message === 'No saved albums has been found.') {
+    showDetail = true;
+  }
   return (
     <div className="error-container">
-      <h1>{message}</h1>
+      <h1 className="error-message">{message}</h1>
+      {showDetail ? (
+        <div className="error-detail">
+          <span>{errorDetail}</span>
+        </div>
+      ) : null}
       <Link to="/">
         <Button type="warning">Try again?</Button>
       </Link>
